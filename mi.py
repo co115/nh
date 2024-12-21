6c4e853c689eeabc5993a835a145cdcd67cde93d
co115 <134004137+co115@users.noreply.github.com>
2024-12-21 11:47:25 +0800

    Update mi.py

--- mi.py
+++ mi.py
@@ -11,10 +11,9 @@ headers = {
 }
 
 try:
-    # 发送 GET 请求
     response = requests.get(B2, headers=headers)
     response.raise_for_status()
-    response_text = response.text  # 获取文本内容
+    response_text = response.text
 
     print("Response text:", response_text)
     
@@ -28,12 +27,10 @@ try:
 
     data = json.loads(response_text)
 
-    # 找到并替换特定字段值
     for site in data.get("sites", []):
         if site.get("key") == "push_agent":
             site["name"] = "请勿相信视频中广告"
 
-    # 修改内容
     data["wallpaper"] = "http://www.kf666888.cn/api/tvbox/img"
     data["logo"] = "./jar/logo.gif"
     data["warningText"] ="否极泰来,好运连连。"
@@ -73,157 +70,8 @@ try:
       "playerType": 1
         }
     ]
-    data["rules"] = [
-        {
-            "name": "暴风",
-            "hosts": [
-                "bfzy",
-                "bfbfvip",
-                "bfengbf"
-            ],
-            "regex": [
-                "#EXTINF.*?\\s+.*?adjump.*?\\.ts"
-            ]
-        },
-        {
-            "name": "量子",
-            "hosts": [
-                "vip.lz",
-                "hd.lz",
-                ".cdnlz"
-            ],
-            "regex": [
-                "#EXT-X-DISCONTINUITY\\r*\\n*#EXTINF:7\\.166667,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-                "#EXT-X-DISCONTINUITY\\r*\\n*#EXTINF:4\\.066667,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-                "17.19"
-            ]
-        },
-        {
-            "name": "非凡",
-            "hosts": [
-                "vip.ffzy",
-                "hd.ffzy",
-                "super.ffzy"
-            ],
-            "regex": [
-                "#EXT-X-DISCONTINUITY\\r*\\n*#EXTINF:6\\.400000,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-                "#EXT-X-DISCONTINUITY\\r*\\n*#EXTINF:6\\.666667,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-                "#EXTINF.*?\\s+.*?1171(057).*?\\.ts",
-                "#EXTINF.*?\\s+.*?6d7b(077).*?\\.ts",
-                "#EXTINF.*?\\s+.*?6718a(403).*?\\.ts",
-                "17.99",
-                "14.45"
-            ]
-        },
-        {
-            "name": "索尼",
-            "hosts": [
-                "suonizy"
-            ],
-            "regex": [
-                "#EXT-X-DISCONTINUITY\\r*\\n*#EXTINF:1\\.000000,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-                "#EXTINF.*?\\s+.*?p1ayer.*?\\.ts",
-                "#EXTINF.*?\\s+.*?\\/video\\/original.*?\\.ts"
-            ]
-        },
-        {
-            "name": "快看",
-            "hosts": [
-                "kuaikan"
-            ],
-            "regex": [
-                "#EXT-X-KEY:METHOD=NONE\\r*\\n*#EXTINF:5,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-                "#EXT-X-KEY:METHOD=NONE\\r*\\n*#EXTINF:2\\.4,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-                "#EXT-X-KEY:METHOD=NONE\\r*\\n*#EXTINF:1\\.467,[\\s\\S]*?#EXT-X-DISCONTINUITY"
-            ]
-        },
-        {
-            "name": "一起看广告",
-            "hosts": [
-                "yqk88"
-            ],
-            "regex": [
-                "18.4",
-                "15.1666",
-                "16.5333",
-                "#EXT-X-DISCONTINUITY\\r*\\n*[\\s\\S]*?#EXT-X-CUE-IN"
-            ]
-        },
-        {
-            "name": "爱坤",
-            "hosts": [
-                "ik"
-            ],
-            "regex": [
-                "#EXT-X-KEY:METHOD=NONE\\r*\\n*#EXTINF:[\\s\\S]*?#EXT-X-DISCONTINUITY"
-            ]
-        },
-        {
-            "name": "磁力广告",
-            "hosts": [
-                "magnet"
-            ],
-            "regex": [
-                "更多",
-                "请访问",
-                "example",
-                "社 區",
-                "x u u",
-                "直 播",
-                "更 新",
-                "社 区",
-                "有趣",
-                "有 趣",
-                "英皇体育",
-                "全中文AV在线",
-                "澳门皇冠赌场",
-                "哥哥快来",
-                "美女荷官",
-                "棋牌",
-                "裸聊",
-                "新片首发",
-                "UUE29"
-            ]
-        },
-        {
-            "name": "火山嗅探",
-            "hosts": [
-                "huoshan.com"
-            ],
-            "regex": [
-                "item_id="
-            ]
-        },
-        {
-            "name": "抖音嗅探",
-            "hosts": [
-                "douyin.com"
-            ],
-            "regex": [
-                "is_play_url="
-            ]
-        },
-        {
-            "name": "农民嗅探",
-            "hosts": [
-                "toutiaovod.com"
-            ],
-            "regex": [
-                "video/tos/cn"
-            ]
-        }
-    ]
 
-    data["ads"] = [
-        "wan.51img1.com",
-        "iqiyi.hbuioo.com",
-        "vip.ffzyad.com",
-        "https://lf1-cdn-tos.bytegoofy.com/obj/tos-cn-i-dy/455ccf9e8ae744378118e4bd289288dd"
-    ]
-
-    # 添加新数据到 "sites" 的第二段
     if "sites" in data:
-        # 如果 "sites" 是一个包含列表的列表，则将其转换为单一的列表
         if isinstance(data["sites"], list) and isinstance(data["sites"][0], list):
             data["sites"] = [item for sublist in data["sites"] for item in sublist]
 
@@ -312,7 +160,6 @@ try:
             "ext":"./json/qx.json"
         }
         
-        # 将 new_site 和 new_site2 插入到特定位置
         data["sites"].insert(0, new_site)
         data["sites"].insert(1, new_site1)
         data["sites"].insert(3, new_site2)
@@ -324,18 +171,15 @@ try:
     else:
         print('"sites" 键不在数据中')
 
-    # 删除指定的键
     keys_to_remove = ["豆瓣","配置","csp_Wogg","csp_Netfixtv","csp_UC","米搜","荐片"]
     data["sites"] = [site for site in data["sites"] if site.get("key") not in keys_to_remove]
     
-    # 将修改后的内容转换为 JSON 字符串，并指定 ensure_ascii=False 以确保汉字和表情符号正常显示
     modified_content = json.dumps(data, indent=2, ensure_ascii=False)
 
-    # 将修改后的 JSON 字符串写入 b2.txt 文件中
     with open('b2.txt', 'w', newline='', encoding='utf-8') as f:
         f.write(modified_content)
 
-    print("修改后的内容已写入到 b2.txt 文件中。")
+    print("已写入。")
 
 except requests.RequestException as e:
     print(f"请求失败: {e}")
